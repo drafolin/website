db7ebefe9b7f4f896a14d282544a45b852adf2c6
Odin Beuchat <66629792+dindin-nibnib@users.noreply.github.com>
2022-08-06 12:18:16 +0200

    better description

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import {useI18n} from "vue-i18n";
-import {watch} from "vue";
+import { useI18n } from "vue-i18n";
+import { watch } from "vue";
 
 const { locale } = useI18n({ useScope: 'global' });
 
 useHead({
   meta: [
-    {name: "description", content: "dindin|nibnib's presentation website."},
-    {name: "charset", content: "utf-8"}
+    { name: "description", content: "Hi there! Am dindin|nibnib!" },
+    { name: "charset", content: "utf-8" }
   ],
   htmlAttrs: {
     lang: locale.value,
@@ -24,9 +24,9 @@ watch(locale, (lang) => {
 </script>
 
 <template>
-	<div>
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
-	</div>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
