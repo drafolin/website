aa79d0aa057d36ef6f1af695195fd0ad236db0a7
Odin Beuchat <odin.beuchat@divtec.ch>
2023-04-14 21:25:39 +0200

    added about me page

---
+++ src/aboutPage.tsx
@@ -0,0 +1,12 @@
+const AboutPage = () => {
+	return <>
+		<h1>About Me</h1>
+		<p>
+			Hi there!
+
+			I'm just a regular nightwing furry who likes to code and make websites (mostly).
+		</p>
+	</>;
+};
+
+export default AboutPage;
