86fa020fb983b2fbc77e1f6244640987cfa5e4cb
Odin Beuchat <odin.beuchat@divtec.ch>
2023-04-14 18:33:48 +0200

    finished desktop first page

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import { Link } from "react-router-dom";
 const Index = () => {
 	return (
 		<>
-			<h1>
-				Your local tech derg <strong>dindin</strong> here!
-			</h1>
+			<div className="hero">
+				<h1>
+					Your local tech derg <strong>dindin</strong> here!
+				</h1>
+				<p>
+					Hi and welcome to my website! Have a look around!
+				</p>
+				<Link to="/about">About me</Link>
+			</div>
 		</>
 	);
 };
